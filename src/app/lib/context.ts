caf2f91f804801c22a01e294e4e95e922e574202
kylebatucal <31263871+kylebatucal@users.noreply.github.com>
2024-07-31 03:15:37 -0700

    Fix prettier bug

--- src/app/lib/context.ts
+++ src/app/lib/context.ts
@@ -1,5 +1,6 @@
 import { createContext } from 'react'
 import { initalizeSettings } from './settings'
 
-export const soundContext = createContext<Record<string, ({}) => void>>({})
+// prettier-ignore
+export const soundContext = createContext<Record<string, ({}?) => void>>({})
 export const settingsContext = createContext(initalizeSettings())
