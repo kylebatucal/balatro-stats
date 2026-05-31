757316442b2ab59232879cfd430d8c537544435a
kylebatucal <31263871+kylebatucal@users.noreply.github.com>
2026-05-31 13:54:08 -0700

    Remove vestigial auth dependency

--- src/components/sections/navbar/NavBar.tsx
+++ src/components/sections/navbar/NavBar.tsx
@@ -4,7 +4,6 @@ import { Options } from '../popups/Options'
 import { useState } from 'react'
 import { Updater } from 'use-immer'
 import Button from '../../inputs/Button'
-import Login from '../popups/LogIn'
 
 export default function NavBar({
   profile,
@@ -13,7 +12,6 @@ export default function NavBar({
   profile: Profile
   setter: Updater<Record<string, { label: string; enabled: boolean }>>
 }) {
-  const [shareOpen, setShareOpen] = useState(false)
   const [optionsOpen, setOptionsOpen] = useState(false)
 
   return (
@@ -25,18 +23,6 @@ export default function NavBar({
         </>
 
         <div className={styles.buttons}>
-          {/* <Button
-            name={'Share'}
-            style={{
-              padding: '0',
-              minWidth: 'initial',
-              boxShadow: '0 0 transparent',
-            }}
-            callback={() => {
-              setShareOpen(true)
-            }}
-            underline={true}
-          /> */}
           <Button
             name={'Options'}
             style={{
@@ -51,7 +37,6 @@ export default function NavBar({
           />
         </div>
       </div>
-      {shareOpen && <Login setter={setShareOpen} />}
       {optionsOpen && (
         <Options settingsSetter={setter} activeSetter={setOptionsOpen} />
       )}
